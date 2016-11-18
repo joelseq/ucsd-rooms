1607031e40f9c40ce271a27486c5ff98e40a6225
Vincent <vincent.oddrepublic@gmail.com>
2016-11-18 14:38:20 -0800

    Minor frontend updates

--- app/components/LocationResults.jsx
+++ app/components/LocationResults.jsx
@@ -23,18 +23,27 @@ export default class LocationResults extends React.Component {
     // Order by room
     this.props.locations.map(opening => {
       tmp.push(
-        <div key={opening._id} className="RoomResult">
-          <h4>{opening.room.name}</h4>
-          <p>Free from {formatTime(opening.start)} to {formatTime(opening.end)}</p>
+        <div key={opening._id} className="RoomResult RoomResult--individual">
+          <div className="RoomResult--title">
+            <h4>{opening.room.name}</h4>
+          </div>
+          <div className="RoomResult--time">
+            <p><span className="emphasis">{formatTime(opening.start)} </span>
+              to 
+              <span className="emphasis"> {formatTime(opening.end)}</span>
+            </p>
+          </div>
         </div>
       )
     });
     return (
       <div key={this.props.locations[0]._id} 
         className="Results"
-        onClick={() => this.handleShow()}
       >
-        <div className="RoomResult RoomName">
+        <div 
+          className="RoomResult RoomName"
+          onClick={() => this.handleShow()}
+        >
           <h3>{this.props.locations[0].room.name.split(' ')[0]}</h3>
           <p>{this.props.locations.length} rooms available</p>
 
@@ -44,6 +53,14 @@ export default class LocationResults extends React.Component {
         </div>
 
         <div className={hidden}>
+          <div className="RoomResult RoomResult--header">
+            <span className="left">
+              Room
+            </span>
+            <span className="right">
+              Available
+            </span>
+          </div>
           {tmp}
         </div>
       </div>
