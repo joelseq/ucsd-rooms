7a4b6c8f4f34cc5413e5d6990b43b807310324ca
Joel Sequeira <joelseq96@gmail.com>
2016-12-04 22:05:48 -0800

    Add EndOfQuarter Component

---
+++ app/components/EndOfQuarter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default function EndOfQuarter(props) {
+  return (
+
+    <div className="Home">
+      <div className="Home--content">
+        <div className="Wrapper">
+          <h2>Find empty classrooms at UCSD!</h2>
+          <p>It's the end of the quarter! UCSD Room Finder will return with the updated classrooms next quarter. Thank you for using UCSD Room Finder!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
