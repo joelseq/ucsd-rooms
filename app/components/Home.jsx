39e87eb263b2d8c00fba614753c6f50257d04e66
Joel Sequeira <joelseq96@gmail.com>
2017-09-11 14:14:53 -0700

    Change EOQ

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -42,7 +42,7 @@ export default class Home extends Component {
 
   render() {
     // Put this to true if it's the end of the quarter
-    const eoq = true;
+    const eoq = false;
 
     if (eoq) {
       return <EndOfQuarter />;
