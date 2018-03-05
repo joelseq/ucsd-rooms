33e42c139557071760800e62b50e339dcf2db266
Joel Sequeira <joelseq96@gmail.com>
2018-03-05 14:50:25 -0800

    Configure sass and add copy over old styles

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Import app styles
+import './styles/styles.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
